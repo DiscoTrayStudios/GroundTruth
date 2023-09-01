8bb110953cfd552f20542a0d17a0fb845b919930
brimstonetrader <106700187+brimstonetrader@users.noreply.github.com>
2023-09-01 09:28:22 -0500

    Q to leave journal

--- GameDevFinal/Assets/Scripts/testJournal.cs
+++ GameDevFinal/Assets/Scripts/testJournal.cs
@@ -117,6 +117,12 @@ public class testJournal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            if (openedNotebook.activeSelf) {
+                bookCloseSound.Play();
+                userInterface.SetActive(true);  
+                openedNotebook.SetActive(false);
+            }
+        }
     }
 }
